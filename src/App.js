7df80000513470a65c1e8c990af58d0ae0519720
Sharon-Anita-Omonge <sharonanita2021@gmail.com>
2024-09-10 10:12:44 +0300

    fav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
+import { Route, Routes } from "react-router";
 
 function App() {
 	return (
 		<div className='App'>
 			<Navbar />
+			<Routes>
+				<Route />
+			</Routes>
 		</div>
 	);
 }
